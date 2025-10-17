1ce54bb5f17d9a08b18c7fcfefc3eeae459eda2d
Christopher Raheb <craheb123@gmail.com>
2025-10-16 22:18:17 -0700

    Step 1 complete: created all html elements with typescript.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import "./style.css";
 
-document.body.innerHTML = `
-  <div id="flexbox">
-    <h1>Sticker Sketchpad</h1>
-    <canvas id="sketchpad"></canvas>
-  </div>
-`;
+const FLEXBOX = document.createElement("div");
+FLEXBOX.id = "flexbox";
+document.body.appendChild(FLEXBOX);
+
+const TITLE = document.createElement("h1");
+TITLE.textContent = "Sticker Sketchpad";
+FLEXBOX.appendChild(TITLE);
+
+const CANVAS = document.createElement("canvas");
+CANVAS.id = "sketchpad";
+CANVAS.width = 256;
+CANVAS.height = 256;
+FLEXBOX.appendChild(CANVAS);
